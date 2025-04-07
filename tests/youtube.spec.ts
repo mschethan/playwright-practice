83c6a7901cf93fb9790f2f91d56d0c71b3c6e4c6
mschethan <mschethan31@gmail.com>
2025-04-07 20:32:44 +0530

    first video click test

--- tests/youtube.spec.ts
+++ tests/youtube.spec.ts
@@ -17,3 +17,32 @@ test('Search for a video', async ({ page }) => {
     await expect(VideoContents).toBeVisible();
 
 })
+
+test ('Click on a video', async ({ page }) => {
+    
+    //Go to Youtube
+    await page.goto('https://www.youtube.com')
+
+    //Search for a video
+    const SearchBar = page.getByPlaceholder('Search');
+    await SearchBar.fill('Playwright Tutorial');
+    await SearchBar.press('Enter');
+
+    //Wait for the search results to load
+    const VideoContents = page.locator("//ytd-search[@role='main']");
+
+    const FirstVideo = page.locator('ytd-video-renderer');
+    await FirstVideo.first()    
+    await expect(FirstVideo).toBeVisible();
+
+    //First Video Title
+    const VideoTitle = page.locator("(//a[@id='video-title'])[1]");
+    await expect(VideoTitle).toBeVisible();
+    await VideoTitle.innerText();
+    console.log(await VideoTitle.innerText());
+
+    //Click on the first video
+    await VideoTitle.click();
+    await expect(page).toHaveURL(/\/watch\?v=/);
+    await expect(VideoTitle).toBeVisible();
+})
